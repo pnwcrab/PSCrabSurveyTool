cf7b08d1b9750aed022a6d71d75c20700df4b13f
Daniel Sund <dmsund@gmail.com>
2022-06-22 17:04:36 -0700

    Update Crab Survey Tool_Hex Grid Generation.R

--- Crab Survey Tool_Hex Grid Generation.R
+++ Crab Survey Tool_Hex Grid Generation.R
@@ -16,16 +16,7 @@ ps <- ps %>%
   dplyr::select(AreaName, geometry)
 
 ## Read in crab management regions shapefile
-ps_cmr <- st_read("./Input/Puget_Sound_Crab_Regions.shp")%>%
-  st_make_valid()
-
-ps_cmr$Region <- ps_cmr$AreaName 
-ps_cmr$Subregion <- ps_cmr$SubareaNam
-
-ps_cmr <- ps_cmr %>%
-  dplyr::select(Region, Subregion, geometry)
-
-## Read in crab management regions shapefile
+### This file has the 26A E and W designations. 
 ps_crab_mfsf <- st_read("./Input/Puget_Sound_Crab_MFSF_CA_Modifications.shp")%>%
   st_make_valid()
 
@@ -42,18 +33,16 @@ ps_mfsf <- ps_mfsf %>%
   dplyr::select(CatchArea, geometry) %>%
   filter(CatchArea != "26A")
 
-## Bring in the crab exclusion areas for the State Commercial Fleet.
-# ps_crab_ex <- st_read("./Input/Puget_Sound_Crab_Exclusion_Areas.shp") %>%
-#   st_make_valid()
-# 
-# ps_crab_ex$ExclusionArea <- ps_crab_ex$AreaName
-# 
-# ps_crab_ex <- ps_crab_ex %>%
-#   dplyr::select(ExclusionArea, geometry)
-
 ## Create a master PS Crab management spatial layers.
-ps.management <- st_join(ps_mfsf, ps_cmr)
-
+ps.management <- ps_mfsf %>%
+  mutate(Region = case_when(CatchArea %in% c("26D", "28A", "28B", "28C", "28D") ~ "6", 
+                            CatchArea %in% c("26B", "26C") ~ "4", 
+                            CatchArea %in% c("25C", "27A", "27B", "27C") ~ "5",
+                            CatchArea %in% c("25B", "25D", "26A-W") ~ "2W",
+                            CatchArea %in% c("24A", "24B", "24C", "24D", "26A-E") ~ "2E",
+                            CatchArea %in% c("23D", "25A", "25E") ~ "3",
+                            CatchArea %in% c("23A", "23B", "23C", "23D", 29) ~ "3",
+                            CatchArea %in% c("20A", "20B", "21A", "21B", "22A", "22B") ~ "1"))
 
 # Make hex grid in footprint of Puget Sound Management layer shapefile 
 ps_hex_2000 <- st_make_grid(ps, 
@@ -67,12 +56,14 @@ ps_hex_2000 <- ps_hex_2000 %>%
 
 ## Join together the management information with the hex cells.
 ## Roll together from the largest spatial unit to the smallest.
-ps_crab_sg <- st_intersection(ps_hex_2000, ps.management)
+ps_crab_sg <- st_join(ps_hex_2000, ps.management) %>%
+  filter(is.na(Region) == FALSE)
 
 ps_crab_sg <- ps_crab_sg %>%
   filter(is.na(ID) == FALSE)
 
 st_write(ps_crab_sg, "./Output/Puget Sound_Hexagonal Survey Grid_2000ft.shp")
+saveRDS(ps_crab_sg, "./Output/Puget Sound_Hexagonal Survey Grid_2000ft.rds")
 
 ps.map <- mapview(ps_crab_sg,
                   zcol =  "Region",
